cfc7cdb87208eec90463a986a47783af49e6c694
Николай Распопин <off_on@bk.ru>
2023-03-01 17:33:58 +0800

    make strukture, add user model, shemas, endpoints, db config

---
+++ app/core/config.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+from pydantic import BaseSettings, EmailStr
+
+
+class Settings(BaseSettings):
+    app_title: str = 'Сервис Благотворительного фонда поддержки котиков QRKot'
+    app_description: str = 'онд собирает пожертвования на различные целевые проекты: на медицинское обслуживание нуждающихся хвостатых, на обустройство кошачьей колонии в подвале, на корм оставшимся без попечения кошкам — на любые цели, связанные с поддержкой кошачьей популяции.'
+    database_url: str
+    secret: str = 'SECRET'
+    first_superuser_email: Optional[EmailStr] = None
+    first_superuser_password: Optional[str] = None
+
+    class Config:
+        env_file = '.env'
+
+
+settings = Settings()
